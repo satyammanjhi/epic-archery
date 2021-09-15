47219dfa9bce89864db1d3fa829362b276bb0785
satyammanjhi <87596271+satyammanjhi@users.noreply.github.com>
2021-09-15 18:14:07 +0530

    Update computerplayer.js

--- computerplayer.js
+++ computerplayer.js
@@ -8,7 +8,7 @@ class computerPlayer{
         this.body=Bodies.rectangle(x,y,width,height,options);
         this.width=width;
         this.height=height;
-        this.image=loadImage("./assets/player.png");
+        this.image=loadImage("player.png");
 
         World.add(world,this.body);
     }
